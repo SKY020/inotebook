388940648154d7c5fbd31342c3e8b120c99f9367
Shiv Kumar Yadav <shivamyadavsy020@gmail.com>
2022-08-31 21:36:57 +0530

    initialise

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const connectToMongo = require('./db');
+const express = require('express')
+
+connectToMongo();
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Shiv!')
+})
+
+app.get('/api/v1/login', (req, res) => {
+    res.send('Hello login!')
+  })
+  
+  app.get('/api./v1/signup', (req, res) => {
+    res.send('Hello signup!')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
